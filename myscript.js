ff7a2715b88b8ea2b7480119f56d05da9befe25a
Will <elkanahbrandt@icloud.com>
2022-11-26 22:29:18 +0000

    first commit

---
+++ myscript.js
@@ -0,0 +1,116 @@
+
+
+        
+    // These are string variables.
+    
+            var bird = "Parrot"
+            console.log (bird);
+            
+    
+            var boys = "Joshua"
+            console.log(boys);
+            
+    
+            var girls = "Bonita"
+            console.log(girls);
+           
+            var fruit = "Apple"
+            console.log(fruit);
+          
+    
+            var grains ="Corn"
+            console.log(grains);
+            
+    
+    
+    // These are integer variables
+            var pen = 20;
+            console.log(pen);
+          
+    
+            var erazor = 15
+            console.log(erazor);
+            
+    
+            var crayon =10
+            console.log(crayon);
+       
+    
+            var ruler = -5
+            console.log(ruler);
+          
+    
+            var pencil = -2
+            console.log(pencil);
+           
+    
+    
+    // These are float variables
+     var aduanepa= 5.6;
+            console.log(aduanepa);
+            document.write(aduanepa);
+    
+            var  kfc = 20.5;
+            console.log( kfc);
+            document.write(kfc);
+    
+            var asanka =30.5;
+            console.log(asanka);
+            document.write(asanka);
+            
+            var papaye =50.5;
+            console.log(papaye);
+            document.write(papaye);
+    
+            var laspamas = 10.5;
+            console.log(laspamas);
+            document.write(laspamas);
+    
+            
+    // These are boolean variables
+            var myAge = 17;
+    
+            var boyAge = 20;
+            console.log(myAge > boyAge);
+            document.write(myAge > boyAge);
+    
+            var hisAge = 12;
+          
+            var herAge =22 ;
+            console.log(hisAge < herAge);
+            document.write(hisAge < herAge);
+    
+            var DanAge = 42;
+    
+            var WillsAge =34;
+            console.log(DanAge < WillsAge);
+            document.write(DanAge < WillsAge);
+            
+            var JoanAge= 10;
+    
+            var BrittaAge = 12;
+            console.log(JoanAge < BrittaAge);
+            document.write(JoanAge < BrittaAge);
+    
+            var ClementAge = 4;
+    
+            var JessyAge = 15;
+            console.log(ClementAge < JessyAge);
+            document.write(ClementAge < JessyAge);
+
+    
+     
+    
+            
+    
+    
+    
+    
+    
+            
+    
+    
+    
+    
+    
+    
